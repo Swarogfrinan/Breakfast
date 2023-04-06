04a149473f4e924961ad90facc4e14cfbc49c472
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-06 14:04:42 +0300

    Create DataForCell.swift

---
+++ Breakfast/Module/ViewModel/DataForCell.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct DataForCell {
+    let recipeID: String
+    let name: String
+    let imageLink: String
+    let lastUpdated: String
+    let description: String?
+    let instructions: String
+    let date: Double
+}
