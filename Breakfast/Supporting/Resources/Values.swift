775e19bd96e989c333461a2e64f8f803be8b5692
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-06 14:04:09 +0300

    Update Constants in Values
    Added Inset, added design constants

--- Breakfast/Supporting/Resources/Values.swift
+++ Breakfast/Supporting/Resources/Values.swift
@@ -1,6 +1,22 @@
 import UIKit
 
 struct Constants {
+    
+    struct Design {
+        static let cornerRadiusError = CGFloat(22)
+        static let cornerRadiusMain = CGFloat(15)
+        static let spacingMain = CGFloat(22)
+        static let basicInset = CGFloat(20)
+        static let borderWidth = CGFloat(1)
+    }
+    
+    struct Inset {
+        static let huge = CGFloat(30)
+        static let classic = CGFloat(20)
+        static let small = CGFloat(18)
+        static let tiny = CGFloat(15)
+    }
+    
     struct API {
         static let baseURL = "https://test.kode-t.ru/"
     }
