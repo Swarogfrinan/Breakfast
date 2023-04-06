b58ccc172abab147b3d59918eb84482a5ef35750
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-06 15:50:43 +0300

    Update Properties in RecipeViewController

---
+++ Breakfast/Module/RecipeScreen/View/RecipesViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class RecipesViewController: UIViewController {
+    
+    // MARK: - Views
+    let tableView = UITableView()
+    let searchController = UISearchController()
+    
+    // MARK: - Properties
+    
+    let viewModel: RecipeViewModel
+    private var filteredRecipes: [TableCellViewModel] = []
+    private var currentSearchCase = SearchCase.all
+    
+    private var currentSortCase = SortCase.date {
+        didSet {
+            filteredRecipes = viewModel.sortRecipesBy(sortCase: currentSortCase, recipes: filteredRecipes)
+            tableView.reloadData()
+        }
+    }
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
