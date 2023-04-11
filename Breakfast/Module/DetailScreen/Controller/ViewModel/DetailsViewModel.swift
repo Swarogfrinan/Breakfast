aa9426db84cbd873156fc56f70eed30693611922
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-11 14:15:49 +0300

    Added action properties in DetailsViewModel

--- Breakfast/Module/DetailScreen/Controller/ViewModel/DetailsViewModel.swift
+++ Breakfast/Module/DetailScreen/Controller/ViewModel/DetailsViewModel.swift
@@ -1,5 +1,16 @@
 import UIKit
 
+protocol ViewModelCoordinatorDelegate: AnyObject {
+    func viewWillDisappear()
+    func didSelectRecipe(recipeID: String)
+}
+
 final class DetailsViewModel {
     // MARK: - Properties
+    // MARK: - Actions
+    
+    var didReceiveError: ((Error) -> Void)?
+    var didStartUpdating: (() -> Void)?
+    var didFinishUpdating: (() -> Void)?
+    
 }
