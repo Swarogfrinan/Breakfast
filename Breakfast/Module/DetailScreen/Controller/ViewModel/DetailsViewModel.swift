2ca1c4b5ac431e2d868f38cfd5c9728cfbbc0474
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-11 16:42:36 +0300

    Update DetailsViewModel with init()

--- Breakfast/Module/DetailScreen/Controller/ViewModel/DetailsViewModel.swift
+++ Breakfast/Module/DetailScreen/Controller/ViewModel/DetailsViewModel.swift
@@ -7,10 +7,20 @@ protocol ViewModelCoordinatorDelegate: AnyObject {
 
 final class DetailsViewModel {
     // MARK: - Properties
+    
+    private let repository: Repository
+    private let recipeID: String
+    
     // MARK: - Actions
     
     var didReceiveError: ((Error) -> Void)?
     var didStartUpdating: (() -> Void)?
     var didFinishUpdating: (() -> Void)?
     
+    // MARK: - Initialization
+    init(repository: Repository, recipeID: String) {
+        self.repository = repository
+        self.recipeID = recipeID
+    }
 }
+
